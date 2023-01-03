4021be08a004d1d7a5a3e6180e8b3f0132f3e8ae
Bradley Lewter <rakugo1@gmail.com>
2023-01-03 11:20:44 -0500

    Fixed growing paragraph issue and updated relevant pages. Added default text for certain elements in creators.

---
+++ react-app/src/components/Splash/BookBlock.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux"
+import './Splash.css'
+import * as bookActions from '../../store/book'
+import * as bookshelfActions from '../../store/bookshelf'
+import * as creatorActions from '../../store/creator'
+
+const BookBlock = ({book}) => {
+  const dispatch = useDispatch();
+  const dispatchSingle = (bookId) => {
+    dispatch(bookActions.getSingleBookThunk(bookId))
+  }
+
+  return  (
+    <>
+    <NavLink to={`/books/${book.id}`} key={book.id} onClick={()=> dispatchSingle(book.id)}>
+        <div className="splash_discover_reccomendations_books">
+          <img src={book.Cover} alt={book.title} />
+          <div className="splash_discover_reccomendations_details">
+            <div className="splash_discover_reccomendations_title">
+              <h2>{book.title}</h2>
+            </div>
+            <div className="splash_discover_reccomendations_averagerating">
+              <h3>AVG. Rating: {book.AverageRating}</h3>
+            </div>
+            <div className="splash_discover_reccomendations_averagerating">
+              <h3>{book.Creators.map((creator)=> (
+                <span className="splash_creator_list">
+                {creator.role}: {creator.name}
+                </span>
+              ))}</h3>
+            </div>
+            <div className="splash_discover_reccomendations_summary">
+              <p id="splash_discover_reccomendations_summary_growing_paragraph">{book.summary}</p>
+            </div>
+          </div>
+        </div>
+    </NavLink>
+    </>
+      )
+}
+
+export default BookBlock
--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -5,6 +5,7 @@ import './Splash.css'
 import * as bookActions from '../../store/book'
 import * as bookshelfActions from '../../store/bookshelf'
 import * as creatorActions from '../../store/creator'
+import BookBlock from './BookBlock.js'
 
 const Splash = ()=>{
   const dispatch = useDispatch();
@@ -46,33 +47,33 @@ const Splash = ()=>{
     }
   }
   if (UserBooks && UserBooks.length > 1) {
-      RenderElement = UserBooks.map((book)=>
-    (
-      <NavLink to={`/books/${book.id}`} key={book.id} onClick={()=> dispatchSingle(book.id)}>
-          <div className="splash_discover_reccomendations_books">
-            <img src={book.Cover} alt={book.title} />
-            <div className="splash_discover_reccomendations_details">
-              <div className="splash_discover_reccomendations_title">
-                <h2>{book.title}</h2>
-              </div>
-              <div className="splash_discover_reccomendations_averagerating">
-                <h3>AVG. Rating: {book.AverageRating}</h3>
-              </div>
-              <div className="splash_discover_reccomendations_averagerating">
-                <h3>{book.Creators.map((creator)=> (
-                  <span className="splash_creator_list">
-                  {creator.role}: {creator.name}
-                  </span>
-                ))}</h3>
-              </div>
-              <div className="splash_discover_reccomendations_summary">
-                <p className="growing_paragraph">{book.summary}</p>
-              </div>
-            </div>
-          </div>
-      </NavLink>
-        )
-  )}
+      RenderElement = UserBooks.map((book)=> <BookBlock book={book} /> )}
+  //   (
+  //     <NavLink to={`/books/${book.id}`} key={book.id} onClick={()=> dispatchSingle(book.id)}>
+  //         <div className="splash_discover_reccomendations_books">
+  //           <img src={book.Cover} alt={book.title} />
+  //           <div className="splash_discover_reccomendations_details">
+  //             <div className="splash_discover_reccomendations_title">
+  //               <h2>{book.title}</h2>
+  //             </div>
+  //             <div className="splash_discover_reccomendations_averagerating">
+  //               <h3>AVG. Rating: {book.AverageRating}</h3>
+  //             </div>
+  //             <div className="splash_discover_reccomendations_averagerating">
+  //               <h3>{book.Creators.map((creator)=> (
+  //                 <span className="splash_creator_list">
+  //                 {creator.role}: {creator.name}
+  //                 </span>
+  //               ))}</h3>
+  //             </div>
+  //             <div className="splash_discover_reccomendations_summary">
+  //               <p id="splash_discover_reccomendations_summary_growing_paragraph">{book.summary}</p>
+  //             </div>
+  //           </div>
+  //         </div>
+  //     </NavLink>
+  //       )
+  // )}
 
 
 
